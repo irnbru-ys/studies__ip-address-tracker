bffc1450660aeb126539e1a1af1ddd6f5ada24db
Sergei <yuriev.s.v.dev.it@gmail.com>
2024-01-21 17:24:56 +0300

    data output

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import { validateIp } from './helpers';
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('.search-bar__btn');
 
+const ipInfo = document.querySelector('#ip');
+const locationInfo = document.querySelector('#location');
+const timezoneInfo = document.querySelector('#timezone');
+const ispInfo = document.querySelector('#isp');
+
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
 
@@ -12,7 +17,7 @@ function getData() {
 		  `https://geo.ipify.org/api/v2/country?apiKey=at_rjFOXHiluNgBpHANSFljflUjpO0y9&ipAddress=${ipInput.value}`,
     )
 		  .then((responce) => responce.json())
-		  .then(console.log);
+		  .then(data => setInfo(data));
   }
 }
 
@@ -20,4 +25,12 @@ function handleKey(e) {
   if (e.key === 'Enter') {
     getData();
   }
+}
+
+function setInfo(mapdata) {
+	console.log(mapdata)
+	ipInfo.innerText = mapdata.ip;
+	locationInfo.innerText = mapdata.location.country + ' ' + mapdata.location.region;
+	timezoneInfo.innerText = mapdata.location.timezone;
+	ispInfo.innerText = mapdata.isp;
 }
