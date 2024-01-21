1bc8cc745fc06767da3cdc8e52de84a15e690983
Sergei <yuriev.s.v.dev.it@gmail.com>
2024-01-21 16:06:44 +0300

    process user input

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.log('Hello');
+const ipInput = document.querySelector('.search-bar__input');
+const btn = document.querySelector('.search-bar__btn');
+
+btn.addEventListener('click', getData);
+ipInput.addEventListener('keydown', handleKey);
+
+function getData() {
+  fetch(
+    `https://geo.ipify.org/api/v2/country?apiKey=at_rjFOXHiluNgBpHANSFljflUjpO0y9&ipAddress=${ipInput.value}`,
+  )
+    .then((responce) => responce.json())
+    .then(console.log);
+}
+
+function handleKey(e) {
+  if (e.key === 'Enter') {
+    getData();
+  }
+}
