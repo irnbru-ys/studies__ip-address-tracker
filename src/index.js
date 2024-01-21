27a4896f58f1ddfe3e5db3db13898e8b1f6fcdb8
Sergei <yuriev.s.v.dev.it@gmail.com>
2024-01-21 18:10:38 +0300

    add a map to the page

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
-import { validateIp } from './helpers';
+import 'leaflet/dist/leaflet.css';
+import L from 'leaflet';
+import { addTileLayer, validateIp } from './helpers';
+import icon from '../images/icon-location.svg';
 
 const ipInput = document.querySelector('.search-bar__input');
 const btn = document.querySelector('.search-bar__btn');
@@ -8,8 +11,24 @@ const locationInfo = document.querySelector('#location');
 const timezoneInfo = document.querySelector('#timezone');
 const ispInfo = document.querySelector('#isp');
 
+const mapArea = document.querySelector('.map');
+
 btn.addEventListener('click', getData);
 ipInput.addEventListener('keydown', handleKey);
+
+const markerIcon = L.icon({
+  iconUrl: icon,
+  iconSize: [30, 40],
+});
+
+const map = L.map(mapArea, {
+  center: [51.505, -0.09],
+  zoom: 13,
+});
+
+addTileLayer(map);
+
+L.marker([51.505, -0.09], { icon: markerIcon }).addTo(map);
 
 function getData() {
   if (validateIp(ipInput.value)) {
@@ -17,7 +36,7 @@ function getData() {
 		  `https://geo.ipify.org/api/v2/country?apiKey=at_rjFOXHiluNgBpHANSFljflUjpO0y9&ipAddress=${ipInput.value}`,
     )
 		  .then((responce) => responce.json())
-		  .then(data => setInfo(data));
+		  .then((data) => setInfo(data));
   }
 }
 
@@ -28,9 +47,9 @@ function handleKey(e) {
 }
 
 function setInfo(mapdata) {
-	console.log(mapdata)
-	ipInfo.innerText = mapdata.ip;
-	locationInfo.innerText = mapdata.location.country + ' ' + mapdata.location.region;
-	timezoneInfo.innerText = mapdata.location.timezone;
-	ispInfo.innerText = mapdata.isp;
+  console.log(mapdata);
+  ipInfo.innerText = mapdata.ip;
+  locationInfo.innerText = `${mapdata.location.country} ${mapdata.location.region}`;
+  timezoneInfo.innerText = mapdata.location.timezone;
+  ispInfo.innerText = mapdata.isp;
 }
